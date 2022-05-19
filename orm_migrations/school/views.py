960e04a8a8fe345a5b4650fc4775f93dac3fb103
Maria Abdullaeva <mashkoyami@gmail.com>
2022-05-19 14:25:47 +0300

    first

---
+++ orm_migrations/school/views.py
@@ -0,0 +1,20 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student, Teacher
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    students = Student.objects.all().order_by('group')
+    teachers = Teacher.objects.all()
+    context = {
+        'students': students,
+        'teachers': teachers
+    }
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
